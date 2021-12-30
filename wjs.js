87b2674912263a5b3df327ee003d5626a941b67d
kumar113 <aman.kumar@knoldus.com>
2021-12-30 13:30:34 +0530

    Adding HTML, CSS and Js files

---
+++ wjs.js
@@ -0,0 +1,9 @@
+function reviewForm() {
+    var fname = document.getElementById("take_input").value;
+    fname = fname.trim();
+    if(fname == ""){
+        alert("*Name field is required*");
+        document.getElementById("take_input").value="";
+        window.location.href="main.html";
+    } 
+}
